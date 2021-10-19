df46c8bd68057013021e901d6211d5f30b5f2415
imjoseangel <josea.munoz@gmail.com>
2021-10-19 19:52:49 +0200

    add lib

--- cosmosdb/__init__.py
+++ cosmosdb/__init__.py
@@ -14,42 +14,12 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.mgmt.cosmosdb import CosmosDBManagementClient
 
+from lib.util import parameters, account
+
 
 def _format(content):
     return json.dumps(content.serialize(keep_readonly=True), indent=4,
                       separators=(',', ': '))
-
-
-def parameters(req, param):
-
-    object_id = req.params.get(param)
-
-    if not object_id or object_id is None:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            object_id = req_body.get(param)
-
-    return object_id
-
-
-def account(req, param):
-
-    account = req.params.get(param)
-    group_name = req.params.get('resourcegroup')
-
-    if not account or not group_name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            account = req_body.get(param)
-            group_name = req_body.get('resourcegroup')
-
-    return account, group_name
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
